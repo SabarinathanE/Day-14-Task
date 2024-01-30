e1f19155dc8a05ab897f4fc07dd5477293b2138c
SabarinathanE <154896658+SabarinathanE@users.noreply.github.com>
2024-01-30 22:49:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+setTimeout((timer) => {
+    document.getElementById("main").innerText = "10"
+    setTimeout((timer) => {
+    document.getElementById("main").innerText = "9"
+    setTimeout((timer) => {
+    document.getElementById("main").innerText = "8"
+    setTimeout((timer) => {
+    document.getElementById("main").innerText = "7"
+    setTimeout((timer) => {
+    document.getElementById("main").innerText = "6"
+    setTimeout((timer) => {
+    document.getElementById("main").innerText = "5"
+    setTimeout((timer) => {
+    document.getElementById("main").innerText = "4"
+    setTimeout((timer) => {
+    document.getElementById("main").innerText = "3"
+    setTimeout((timer) => {
+    document.getElementById("main").innerText = "2"
+    setTimeout((timer) => {
+    document.getElementById("main").innerText = "1"
+    
+    setTimeout((timer) => {
+    document.getElementById("main").innerText = "Happy Independence Day";
+    }, 10);
+    }, 800);
+    }, 800);
+    }, 800);
+    }, 800);
+    }, 800);
+    }, 800);
+    }, 800);
+    }, 800);
+    }, 800);
+    
+    }, 800);
+    
